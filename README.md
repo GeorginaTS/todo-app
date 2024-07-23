34fad30b6278ad9b07602aedc08cba0c313b1091
Georgina <georginatomass@gmail.com>
2024-07-23 15:09:37 +0200

    REgister component working with validations

---
+++ README.md
@@ -0,0 +1,12 @@
+# toDo App
+## Full stack application
+### FRONTEND
+vue + vite + tailwind +router+guards
+### BACKEND
+express+mongoose+jwt+bcrypt
+#### Endpoints
+/user 
+    /login 
+/todos
+/categories
+/status
--- front/src/views/Login.vue
+++ front/src/views/Login.vue
@@ -4,8 +4,8 @@
       <h1>Welcome to toDo APP</h1>
       <br><br><br>
       <RouterLink to="/register">
-      <button class="button_primary">Register</button>
-    </RouterLink>
+        <button class="button_primary">Register</button>
+      </RouterLink>
     </div>
     <div class="w-full flex justify-center items-center">
       <div class="flex flex-col gap-4 div_green1 p-12 rounded-lg w-[80%]">
@@ -22,36 +22,41 @@ export default {
   name: "Login",
   inject: ["serverUrl", "hostUrl"],
   setup() {
-        const userStore = useUserStore()
-        return { userStore }
-    },
+    const userStore = useUserStore()
+    return { userStore }
+  },
   data() {
     return {
       email: "georgina@merit.com",
       password: "123456",
-      token:""
+      token: ""
     }
+  },
+  mounted() {
+    this.userStore.token = ""
+    this.userStore.loggedIn = false
+    this.userStore.user = {}
   },
   methods: {
     async login() {
       try {
         const response = await fetch(`${this.serverUrl}/users/login`, {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json"
-                    },
-                    body: JSON.stringify({"email":this.email, "password": this.password})
-                })
-                const data = await response.json()
-                //console.log(data)
-                this.token = data.token
-                localStorage.setItem("token", data.token)
-                this.userStore.token = this.token
-                this.userStore.loggedIn = true
-                this.userStore.user = data.user
-                this.$router.push('/auth');
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify({ "email": this.email, "password": this.password })
+        })
+        const data = await response.json()
+        //console.log(data)
+        this.token = data.token
+        localStorage.setItem("token", data.token)
+        this.userStore.token = this.token
+        this.userStore.loggedIn = true
+        this.userStore.user = data.user
+        this.$router.push('/auth');
       }
-      catch(error) {
+      catch (error) {
         response.send({ msg: "Error", error: error.message })
       }
     }
