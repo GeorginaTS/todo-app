e7e03f82953150c8b796a2e259c6f2313df16e4b
Georgina <georginatomass@gmail.com>
2024-09-04 15:55:09 +0200

    main.js URL

--- front/src/main.js
+++ front/src/main.js
@@ -8,8 +8,8 @@ const pinia = createPinia();
 createApp(App)
   .use(pinia)
   .use(router)
-  .provide("hostUrl", "https://georgina-todoapp.netlify.app/")
-  .provide("serverUrl", "https://todo-app-ga76.onrender/")
+  .provide("hostUrl", "https://georgina-todoapp.netlify.app")
+  .provide("serverUrl", "https://todo-app-ga76.onrender")
   //.provide("hostUrl", "http://localhost:5173")
   //.provide("serverUrl", "http://localhost:3400/api")
   .mount("#app");
