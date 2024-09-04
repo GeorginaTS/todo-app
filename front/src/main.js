edd564b56638bd7419d0848ad2551e822cbab0dd
Georgina <georginatomass@gmail.com>
2024-09-04 13:32:01 +0200

    change url for deploy in netlifly(front) and render(back)

--- front/src/main.js
+++ front/src/main.js
@@ -1,13 +1,15 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import { router } from './router.js'
-import { createPinia } from 'pinia'
-const pinia = createPinia()
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import { router } from "./router.js";
+import { createPinia } from "pinia";
+const pinia = createPinia();
 
 createApp(App)
-.use(pinia)
-.use(router)
-.provide("hostUrl", "http://localhost:5173")
-.provide("serverUrl", "http://localhost:3400/api")
-.mount('#app')
+  .use(pinia)
+  .use(router)
+  .provide("hostUrl", "https://georgina-todoapp.netlify.app")
+  .provide("serverUrl", "https://todo-app-ga76.onrender.com")
+  //.provide("hostUrl", "http://localhost:5173")
+  //.provide("serverUrl", "http://localhost:3400/api")
+  .mount("#app");
