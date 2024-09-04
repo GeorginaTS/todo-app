6d3962cf4c2482ad88d33aea37242431b6d5e657
Georgina <georginatomass@gmail.com>
2024-09-04 17:49:44 +0200

    serverUrl error fix

--- front/src/main.js
+++ front/src/main.js
@@ -9,7 +9,7 @@ createApp(App)
   .use(pinia)
   .use(router)
   .provide("hostUrl", "https://georgina-todoapp.netlify.app")
-  .provide("serverUrl", "https://todo-app-ga76.onrender")
+  .provide("serverUrl", "https://todo-app-ga76.onrender.com")
   //.provide("hostUrl", "http://localhost:5173")
   //.provide("serverUrl", "http://localhost:3400/api")
   .mount("#app");
